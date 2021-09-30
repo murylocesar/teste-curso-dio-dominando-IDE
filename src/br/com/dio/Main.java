750015575278b4f6e22874ebf334e99c38896b9a
Murylo Cesar <murylocesar2014@gmail.com>
2021-09-30 20:59:33 -0300

    criando class Livro

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -8,11 +8,48 @@ public class Main {
 
         Gato gato = new Gato();
 
+        Livro livro = new Livro("o problema dos 3 corpos", 300);
+
         gato.setName("Jubileu");
-        gato.setCor("Rosa");
+        gato.setCor("Preto");
         gato.setIdade(99);
 
         System.out.println(gato);
+        System.out.println(livro);
 
+    }
+}
+
+class Livro {
+    private String name;
+    private Integer numPaginas;
+
+    public Livro(String name, Integer numPaginas) {
+        this.name = name;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "name='" + name + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
